df19ac65bdf0f6ed1b1028898ba4abbcdf124e64
daniilturpitkacmu <dturpitk@andrew.cmu.edu>
2025-03-09 17:21:59 -0700

    g

--- src/main/java/javatestproj/Main.java
+++ src/main/java/javatestproj/Main.java
@@ -8,6 +8,10 @@ public class Main {
     public static int add(int a, int b){
         return a+b;
     }
+    public static int mod2(int a){
+        int r = a%2;
+        return r;
+    }
     public static void main(String[] args) {
         System.out.println(add(4,5));
         System.out.println(square(9));
